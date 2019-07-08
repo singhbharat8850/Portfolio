090399e589ad8a949ce960a067daea0c4bf1f2c4
singhbharat8850 <singh.bharat8850@gmail.com>
2019-07-09 02:18:43 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed('#tag-line', {
+
+    strings: ["Developer.^500 Dreamer.^500 Achiever. ^500;^100)"],
+
+    typeSpeed: 10,
+
+    startDelay: 1000,
+
+    fadeOut: true,
+
+    fadeOutClass: 'typed-fade-out',
+
+    fadeOutDelay: 500
+
+});
